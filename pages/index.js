112d0e3092634d8346d16332affd2635b6d2c647
Atta Ur Rahman Farooqi <73984528+iamfarooqi@users.noreply.github.com>
2024-01-15 19:46:06 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { ContentProviderGrapes } from 'destack'
 import 'grapesjs/dist/css/grapes.min.css'
 
-export { getStaticProps } from 'destack/build/server'
 
 export default function Page(props) {
     return (
