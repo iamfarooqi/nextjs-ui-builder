5fdf4730f0672d398f73953315d4780c28485c12
iamfarooqi <73984528+iamfarooqi@users.noreply.github.com>
2024-01-15 19:41:56 +0500

    enabled editor in production

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,11 @@
+import { ContentProviderGrapes } from 'destack'
+import 'grapesjs/dist/css/grapes.min.css'
+
 export { getStaticProps } from 'destack/build/server'
-export { ContentProvider as default } from 'destack'
+
+export default function Page(props) {
+    return (
+        <div style={{ height: '100%' }}>
+            <ContentProviderGrapes {...props} showEditorInProd={true} />
+        </div>)
+}
