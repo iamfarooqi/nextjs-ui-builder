feb1fb5cda651cbbb9c4878c09ff0cacd6d1ebe8
iamfarooqi <73984528+iamfarooqi@users.noreply.github.com>
2024-01-17 20:02:24 +0500

    testing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-import { ContentProviderGrapes } from 'destack'
-import 'grapesjs/dist/css/grapes.min.css'
+export { getStaticProps } from 'destack/build/server'
+export { ContentProvider } from 'destack'
 
 
-export default function Page(props) {
+// Import necessary modules
+import { getStaticProps as destackGetStaticProps, ContentProvider as DestackContentProvider } from 'destack';
+
+// Define your custom function to pass as an argument
+const customFunction = () => {
+    // Your custom logic here
+    return 'Custom result';
+};
+
+
+
+// Use ContentProvider from destack with your custom argument
+const ContentProvider = (props) => {
+    console.log('props', props);
     return (
-        <div style={{ height: '100%' }}>
-            <ContentProviderGrapes {...props} showEditorInProd={true} />
-        </div>)
-}
+        <DestackContentProvider>
+            {/* Your component logic here */}
+            <div>
+
+                Hello</div>
+        </DestackContentProvider>
+    );
+};
+
+export default ContentProvider;
